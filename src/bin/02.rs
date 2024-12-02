05dc1603922d5ca470613b1b938bbf0b37e11d78
Brett Porter <49108+brettporter@users.noreply.github.com>
2024-12-02 19:25:05 +1100

    tidy up day 2 code

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -13,68 +13,34 @@ fn parse_input(input: &str) -> IResult<&str, Vec<Vec<i32>>> {
     many1(terminated(separated_list1(tag(" "), i32), opt(newline)))(input)
 }
 
-fn is_safe(report: &Vec<i32>) -> bool {
+fn is_safe(report: &[i32]) -> bool {
     let sign = (report[1] - report[0]).signum();
 
     for i in 1..report.len() {
-        if check_items(report[i], report[i - 1], sign) == false {
+        if (report[i] - report[i - 1]).signum() != sign {
+            return false;
+        } else if !(1..=3).contains(&report[i].abs_diff(report[i - 1])) {
             return false;
         }
     }
     true
 }
 
-fn check_items(cur: i32, prev: i32, sign: i32) -> bool {
-    if (cur - prev).signum() != sign {
-        return false;
-    } else if !(1..=3).contains(&cur.abs_diff(prev)) {
-        return false;
-    }
-    true
-}
-
 fn is_safe_with_dampener(report: &Vec<i32>) -> bool {
-    /*
-    let sign = (report[1] - report[0]).signum();
-    for i in 1..report.len() {
-        if check_items(report[i], report[i - 1], sign) == false {
-            if i > 1 && check_items(report[i], report[i - 2], sign) == false {
-                return false;
-            }
-        }
-    }
-    true
-            */
-
-    if is_safe(report) {
-        return true;
-    }
-
-    for i in 0..report.len() {
-        let mut v = report.clone();
-        v.remove(i);
-        if is_safe(&v) {
-            return true;
-        }
-    }
-    false
+    is_safe(report)
+        || (0..report.len()).any(|i| is_safe(&[&report[0..i], &report[i + 1..]].concat()))
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (_, reports) = parse_input(input).unwrap();
 
-    Some(reports.into_iter().filter(|v| is_safe(v)).count() as u32)
+    Some(reports.iter().filter(|v| is_safe(v)).count() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     let (_, reports) = parse_input(input).unwrap();
 
-    Some(
-        reports
-            .into_iter()
-            .filter(|v| is_safe_with_dampener(v))
-            .count() as u32,
-    )
+    Some(reports.iter().filter(|v| is_safe_with_dampener(v)).count() as u32)
 }
 
 #[cfg(test)]
@@ -99,6 +65,17 @@ mod tests {
         assert_eq!(true, is_safe_with_dampener(&vec![1, 3, 2, 4, 5]));
         assert_eq!(true, is_safe_with_dampener(&vec![8, 6, 4, 4, 1]));
         assert_eq!(true, is_safe_with_dampener(&vec![1, 3, 6, 7, 9]));
+
+        // Input data that failed
+        assert_eq!(true, is_safe_with_dampener(&vec![66, 67, 68, 71, 75]));
+        assert_eq!(
+            false,
+            is_safe_with_dampener(&vec![39, 41, 41, 42, 44, 46, 49, 46])
+        );
+        assert_eq!(
+            true,
+            is_safe_with_dampener(&vec![84, 82, 83, 84, 85, 88, 90])
+        );
     }
 
     #[test]
