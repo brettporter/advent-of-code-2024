58777dfb920f0ff9a54d7b9dd138a5fa3e64b4f9
Brett Porter <49108+brettporter@users.noreply.github.com>
2024-12-02 17:57:52 +1100

    Day 2, part 2 rough version

--- README.md
+++ README.md
@@ -12,8 +12,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
 | [Day 1](./src/bin/01.rs) | `71.9µs` | `65.7µs` |
+| [Day 2](./src/bin/02.rs) | `171.1µs` | `282.2µs` |
 
-**Total: 0.14ms**
+**Total: 0.59ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/02.rs
+++ src/bin/02.rs
@@ -17,16 +17,47 @@ fn is_safe(report: &Vec<i32>) -> bool {
     let sign = (report[1] - report[0]).signum();
 
     for i in 1..report.len() {
-        if (report[i] - report[i - 1]).signum() != sign {
-            return false;
-        }
-
-        if !(1..=3).contains(&report[i].abs_diff(report[i - 1])) {
+        if check_items(report[i], report[i - 1], sign) == false {
             return false;
         }
     }
+    true
+}
 
-    return true;
+fn check_items(cur: i32, prev: i32, sign: i32) -> bool {
+    if (cur - prev).signum() != sign {
+        return false;
+    } else if !(1..=3).contains(&cur.abs_diff(prev)) {
+        return false;
+    }
+    true
+}
+
+fn is_safe_with_dampener(report: &Vec<i32>) -> bool {
+    /*
+    let sign = (report[1] - report[0]).signum();
+    for i in 1..report.len() {
+        if check_items(report[i], report[i - 1], sign) == false {
+            if i > 1 && check_items(report[i], report[i - 2], sign) == false {
+                return false;
+            }
+        }
+    }
+    true
+            */
+
+    if is_safe(report) {
+        return true;
+    }
+
+    for i in 0..report.len() {
+        let mut v = report.clone();
+        v.remove(i);
+        if is_safe(&v) {
+            return true;
+        }
+    }
+    false
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -36,7 +67,14 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (_, reports) = parse_input(input).unwrap();
+
+    Some(
+        reports
+            .into_iter()
+            .filter(|v| is_safe_with_dampener(v))
+            .count() as u32,
+    )
 }
 
 #[cfg(test)]
@@ -54,6 +92,16 @@ mod tests {
     }
 
     #[test]
+    fn test_safe_with_dampener() {
+        assert_eq!(true, is_safe_with_dampener(&vec![7, 6, 4, 2, 1]));
+        assert_eq!(false, is_safe_with_dampener(&vec![1, 2, 7, 8, 9]));
+        assert_eq!(false, is_safe_with_dampener(&vec![9, 7, 6, 2, 1]));
+        assert_eq!(true, is_safe_with_dampener(&vec![1, 3, 2, 4, 5]));
+        assert_eq!(true, is_safe_with_dampener(&vec![8, 6, 4, 4, 1]));
+        assert_eq!(true, is_safe_with_dampener(&vec![1, 3, 6, 7, 9]));
+    }
+
+    #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(2));
@@ -62,6 +110,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
