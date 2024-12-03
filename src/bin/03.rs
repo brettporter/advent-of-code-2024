311e60680d8237d3d4063178a7f8527f65f8694a
Brett Porter <49108+brettporter@users.noreply.github.com>
2024-12-03 17:46:28 +1100

    Day 3, part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,42 +1,102 @@
-use itertools::Itertools;
 use nom::{
-    bytes::complete::{tag, take_until},
-    character::complete::i32,
-    multi::many0,
+    bytes::complete::tag,
+    character::complete::u32,
     sequence::{delimited, preceded, separated_pair},
     IResult,
 };
-use regex::Regex;
 
 advent_of_code::solution!(3);
 
-fn parse_input(input: &str) -> Vec<(u32, u32)> {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    re.captures_iter(input)
-        .map(|c| (c[1].parse().unwrap(), c[2].parse().unwrap()))
-        .collect_vec()
+#[derive(Debug)]
+enum Instruction {
+    DO,
+    DONT,
+    MUL,
 }
 
-// fn parse_input(input: &str) -> IResult<&str, Vec<(i32, i32)>> {
-// This won't work for mul that doesn't match
-// many0(preceded(
-//     take_until("mul"),
-//     preceded(
-//         tag("mul"),
-//         delimited(tag("("), separated_pair(i32, tag(","), i32), tag(")")),
-//     ),
-// ))(input)
-// }
+#[derive(Debug)]
+struct Command {
+    instruction: Instruction,
+    arguments: (u32, u32),
+}
+
+fn parse_input(input: &str) -> Vec<Command> {
+    fn parse_mul_command(cmd: &str) -> IResult<&str, (u32, u32)> {
+        preceded(
+            tag("mul"),
+            delimited(tag("("), separated_pair(u32, tag(","), u32), tag(")")),
+        )(cmd)
+    }
+
+    let mut commands = vec![];
+
+    let mut remaining = input;
+    while !remaining.is_empty() {
+        if remaining.starts_with("do()") {
+            remaining = &remaining[4..];
+            commands.push(Command {
+                instruction: Instruction::DO,
+                arguments: (0, 0),
+            });
+        } else if remaining.starts_with("don't()") {
+            remaining = &remaining[6..];
+            commands.push(Command {
+                instruction: Instruction::DONT,
+                arguments: (0, 0),
+            });
+        } else if let Ok((rest, arguments)) = parse_mul_command(remaining) {
+            remaining = rest;
+            commands.push(Command {
+                instruction: Instruction::MUL,
+                arguments,
+            });
+        } else {
+            remaining = &remaining[1..]
+        }
+    }
+
+    commands
+
+    // many1(preceded(
+    //     opt(char),
+    //     alt((
+    //         pair(tag("do"), tag("()")),
+    //         pair(tag("don't"), tag("()")),
+    //         preceded(
+    //             tag("mul"),
+    //             delimited(tag("("), separated_pair(digit1, tag(","), digit1), tag(")")),
+    //         ),
+    //     )),
+    // ))(input)
+}
+
+fn run(commands: &Vec<Command>) -> u32 {
+    let mut total = 0;
+    let mut enabled = true;
+    for cmd in commands {
+        match cmd.instruction {
+            Instruction::DO => {
+                enabled = true;
+            }
+            Instruction::DONT => {
+                enabled = false;
+            }
+            Instruction::MUL => {
+                if enabled {
+                    total += cmd.arguments.0 * cmd.arguments.1;
+                }
+            }
+        }
+    }
+    total
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
-    // let (_, arguments) = parse_input(input).unwrap();
-    let arguments = parse_input(input);
-
-    Some(arguments.iter().map(|&(v1, v2)| v1 * v2).sum())
+    Some(run(&parse_input(input)))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(run(&parse_input(input)))
 }
 
 #[cfg(test)]
@@ -51,7 +111,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(48));
     }
 }
