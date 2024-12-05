e0103508aa15867b4fa6d98c21712d6519597528
Brett Porter <49108+brettporter@users.noreply.github.com>
2024-12-05 18:29:45 +1100

    Day 5, part 1

---
+++ src/bin/05.rs
@@ -0,0 +1,64 @@
+use nom::{
+    bytes::complete::tag,
+    character::complete::{i32, newline},
+    combinator::opt,
+    multi::{many1, separated_list1},
+    sequence::{separated_pair, terminated},
+    IResult,
+};
+
+advent_of_code::solution!(5);
+
+fn parse_input(input: &str) -> IResult<&str, (Vec<(i32, i32)>, Vec<Vec<i32>>)> {
+    separated_pair(
+        many1(terminated(separated_pair(i32, tag("|"), i32), opt(newline))),
+        newline,
+        many1(terminated(separated_list1(tag(","), i32), opt(newline))),
+    )(input)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (_, (rules, reprints)) = parse_input(input).unwrap();
+
+    let mut total = 0;
+    for reprint in reprints {
+        let mut failed = false;
+        for i in 0..reprint.len() {
+            let r = reprint[i];
+            for &(before, after) in &rules {
+                if before == r {
+                    if reprint[0..i].iter().any(|&v| v == after) {
+                        failed = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if !failed {
+            total += reprint[reprint.len() / 2];
+        }
+    }
+
+    Some(total as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(143));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
