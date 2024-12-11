1e0f31dfea5c71d51e843b900049493f4d0e2001
Brett Porter <49108+brettporter@users.noreply.github.com>
2024-12-11 18:42:32 +1100

    Day 11, part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,3 +1,4 @@
+use fxhash::FxHashMap;
 use nom::{bytes::complete::tag, character::complete::u64, multi::separated_list1, IResult};
 
 advent_of_code::solution!(11);
@@ -6,37 +7,50 @@ fn parse_input(input: &str) -> IResult<&str, Vec<u64>> {
     separated_list1(tag(" "), u64)(input)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     // Naïve approach!
     let (_, numbers) = parse_input(input).unwrap();
 
     Some(count_all_stones(&numbers, 25))
 }
 
-fn count_all_stones(numbers: &[u64], it: i32) -> u32 {
-    numbers.iter().map(|&n| count_stones(n, it)).sum()
+fn count_all_stones(numbers: &[u64], it: i32) -> u64 {
+    let mut memo = FxHashMap::default();
+
+    numbers
+        .iter()
+        .map(|&n| count_stones(n, it, &mut memo))
+        .sum()
 }
 
-fn count_stones(n: u64, it: i32) -> u32 {
+fn count_stones(n: u64, it: i32, memo: &mut FxHashMap<(u64, i32), u64>) -> u64 {
     if it == 0 {
         return 1;
     }
 
-    if n == 0 {
-        count_stones(1, it - 1)
+    if let Some(result) = memo.get(&(n, it)) {
+        return *result;
+    }
+
+    let result = if n == 0 {
+        count_stones(1, it - 1, memo)
     } else {
         let digits = n.ilog10() + 1;
         if digits % 2 == 0 {
             let p = 10u64.pow(digits / 2);
-            count_stones(n / p, it - 1) + count_stones(n % p, it - 1)
+            count_stones(n / p, it - 1, memo) + count_stones(n % p, it - 1, memo)
         } else {
-            count_stones(n * 2024, it - 1)
+            count_stones(n * 2024, it - 1, memo)
         }
-    }
+    };
+    memo.insert((n, it), result);
+    result
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let (_, numbers) = parse_input(input).unwrap();
+
+    Some(count_all_stones(&numbers, 75))
 }
 
 #[cfg(test)]
@@ -63,6 +77,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(65601038650482));
     }
 }
