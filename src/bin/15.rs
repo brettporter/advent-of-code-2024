becec492c27ba032f72fd88cbcb6087b90477313
Brett Porter <49108+brettporter@users.noreply.github.com>
2024-12-15 17:42:47 +1100

    Day 15, part 2 - failed attempt

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,5 +1,3 @@
-use std::io::Empty;
-
 use itertools::Itertools;
 use nom::{
     character::complete::{newline, one_of},
@@ -15,6 +13,8 @@ advent_of_code::solution!(15);
 enum Entity {
     Wall,
     Box,
+    BoxL,
+    BoxR,
     Robot,
     Empty,
 }
@@ -29,9 +29,34 @@ impl Entity {
             _ => unreachable!("Invalid input"),
         }
     }
+
+    fn to_char(&self) -> char {
+        match *self {
+            Self::Wall => '#',
+            Self::Box => 'O',
+            Self::BoxL => '[',
+            Self::BoxR => ']',
+            Self::Empty => '.',
+            Self::Robot => '@',
+        }
+    }
+
+    fn expand(&self) -> [Entity; 2] {
+        match *self {
+            Entity::Box => [Entity::BoxL, Entity::BoxR],
+            Entity::Empty => [Entity::Empty, Entity::Empty],
+            Entity::Robot => [Entity::Robot, Entity::Empty],
+            Entity::Wall => [Entity::Wall, Entity::Wall],
+            _ => unreachable!("Already expanded"),
+        }
+    }
+
+    fn is_box(&self) -> bool {
+        *self == Entity::Box || *self == Entity::BoxL || *self == Entity::BoxR
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Direction {
     Up,
     Down,
@@ -48,6 +73,10 @@ impl Direction {
             '>' => Self::Right,
             _ => unreachable!("Inavlid input"),
         }
+    }
+
+    fn is_vertical(&self) -> bool {
+        *self == Self::Up || *self == Self::Down
     }
 }
 
@@ -95,27 +124,47 @@ fn parse_input(input: &str) -> IResult<&str, (Vec<Vec<Entity>>, Vec<Direction>)>
     separated_pair(parse_map, newline, parse_moves)(input)
 }
 
-fn move_entity(pos: &Coord, direction: Direction, grid: &mut [Vec<Entity>]) -> Coord {
+fn move_entity(
+    pos: &Coord,
+    direction: &Direction,
+    grid: &mut [Vec<Entity>],
+    is_other_half: bool,
+) -> Option<Coord> {
     let next_pos = pos.next_position(&direction);
 
-    if grid[next_pos.y][next_pos.x] == Entity::Box {
-        move_entity(&next_pos, direction, grid);
+    println!(
+        "Try moving {:?} @ {:?} {:?} -> {:?}",
+        grid[pos.y][pos.x], pos, direction, next_pos
+    );
+
+    if grid[next_pos.y][next_pos.x].is_box() {
+        move_entity(&next_pos, direction, grid, false);
     }
 
     if grid[next_pos.y][next_pos.x] == Entity::Empty {
-        grid[next_pos.y][next_pos.x] = grid[pos.y][pos.x];
+        let e = grid[pos.y][pos.x];
+        if e == Entity::BoxL && direction.is_vertical() && !is_other_half {
+            let other_half = pos.next_position(&Direction::Right);
+            if move_entity(&other_half, direction, grid, true).is_none() {
+                return None;
+            }
+        } else if e == Entity::BoxR && direction.is_vertical() && !is_other_half {
+            let other_half = pos.next_position(&Direction::Left);
+            if move_entity(&other_half, direction, grid, true).is_none() {
+                return None;
+            }
+        }
+
+        grid[next_pos.y][next_pos.x] = e;
         grid[pos.y][pos.x] = Entity::Empty;
-        next_pos
+        Some(next_pos)
     } else {
-        *pos
+        None
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (_, (mut grid, moves)) = parse_input(input).unwrap();
-
-    let mut robot_pos = grid
-        .iter()
+fn find_robot(grid: &Vec<Vec<Entity>>) -> Coord {
+    grid.iter()
         .enumerate()
         .find_map(|(y, row)| {
             if let Some(x) = row.iter().position(|&entity| entity == Entity::Robot) {
@@ -124,10 +173,18 @@ pub fn part_one(input: &str) -> Option<u32> {
                 None
             }
         })
-        .unwrap();
+        .unwrap()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (_, (mut grid, moves)) = parse_input(input).unwrap();
+
+    let mut robot_pos = find_robot(&grid);
 
     for m in moves {
-        robot_pos = move_entity(&robot_pos, m, &mut grid);
+        if let Some(p) = move_entity(&robot_pos, &m, &mut grid, false) {
+            robot_pos = p;
+        }
     }
 
     let mut total = 0;
@@ -143,7 +200,42 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (_, (grid, moves)) = parse_input(input).unwrap();
+
+    // TODO: needed?
+    let size = grid.len();
+    assert_eq!(grid[0].len(), size);
+
+    let mut grid = grid
+        .iter()
+        .map(|row| row.iter().flat_map(|entity| entity.expand()).collect())
+        .collect_vec();
+
+    let mut robot_pos = find_robot(&grid);
+
+    for m in moves {
+        if let Some(p) = move_entity(&robot_pos, &m, &mut grid, false) {
+            robot_pos = p;
+        }
+    }
+
+    for row in &grid {
+        for col in row {
+            print!("{}", col.to_char())
+        }
+        println!();
+    }
+
+    let mut total = 0;
+    for (y, row) in grid.iter().enumerate() {
+        for (x, &entity) in row.iter().enumerate() {
+            if entity == Entity::BoxL {
+                total += y * 100 + x;
+            }
+        }
+    }
+
+    Some(total as u32)
 }
 
 #[cfg(test)]
@@ -157,15 +249,13 @@ mod tests {
         ));
         assert_eq!(result, Some(2028));
 
-        let result = part_one(&advent_of_code::template::read_file_part(
-            "examples", DAY, 12,
-        ));
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(10092));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9021));
     }
 }
