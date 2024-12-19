847e6643cda31cb8afae36aa940a3baa1aba8c4e
Brett Porter <49108+brettporter@users.noreply.github.com>
2024-12-19 18:11:41 +1100

    Day 19, part 2

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -25,36 +25,45 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(
         designs
             .iter()
-            .filter(|d| is_possible_design(d, &patterns, &mut cache))
+            .filter(|d| get_possible_designs(d, &patterns, &mut cache) > 0)
             .count() as u32,
     )
 }
 
-fn is_possible_design(
+fn get_possible_designs(
     design: &str,
     patterns: &Vec<&str>,
-    cache: &mut FxHashMap<String, bool>,
-) -> bool {
+    cache: &mut FxHashMap<String, u64>,
+) -> u64 {
     if design.is_empty() {
-        return true;
+        return 1;
     }
     if let Some(&v) = cache.get(&design.to_string()) {
         return v;
     }
 
+    let mut total = 0;
     for p in patterns {
-        if design.starts_with(p) && is_possible_design(&design[p.len()..], patterns, cache) {
-            cache.insert(design.to_string(), true);
-            return true;
+        if design.starts_with(p) {
+            total += get_possible_designs(&design[p.len()..], patterns, cache);
         }
     }
 
-    cache.insert(design.to_string(), false);
-    false
+    cache.insert(design.to_string(), total);
+    total
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let (_, (patterns, designs)) = parse_input(input).unwrap();
+
+    let mut cache = FxHashMap::default();
+
+    Some(
+        designs
+            .iter()
+            .map(|d| get_possible_designs(d, &patterns, &mut cache))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -70,6 +79,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(16));
     }
 }
